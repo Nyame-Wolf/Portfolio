1c38691987f64d7c158ceeaf95b30a4ccf02124d
Priscilla Okoro <oyomepriscilla@gmail.com>
2022-06-13 15:08:42 -0600

    add event listener to hamburger-icon

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const title = document.getElementById("title");
 const mobileLinks = document.getElementById("mobile-links");
 
 openToggle.addEventListener("click", () => {
-  //
+  mobileLinks.classList.toggle("open");
+  openToggle.style.display = "none";
+  title.style.display = "none";
+
 });
