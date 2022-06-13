5dcf469c5968c50a35615bb4e7e742c6879aebdf
Nyame-Wolf <mumenyam@gmail.com>
2022-06-14 00:03:14 +0300

    Activate humburger icon using javascript. Added nav to activate

---
+++ script.js
@@ -0,0 +1,7 @@
+const openToggle = document.getElementById("open-toggle");
+const title = document.getElementById("title");
+const mobileLinks = document.getElementById("mobile-links");
+
+openToggle.addEventListener("click", () => {
+  //
+});
