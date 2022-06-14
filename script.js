72d2bbd334e1c46b5e09f317ebc031d83467e694
Nyame-Wolf <mumenyam@gmail.com>
2022-06-14 13:10:29 +0300

    Style  pop-up for mobile hamburger. Activate close togle.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const openToggle = document.getElementById("open-toggle");
-const title = document.getElementById("title");
-const mobileLinks = document.getElementById("mobile-links");
+const openToggle = document.getElementById('open-toggle');
+const title = document.getElementById('title');
+const mobileLinks = document.getElementById('mobile-links');
 
-openToggle.addEventListener("click", () => {
-  mobileLinks.classList.toggle("open");
-  openToggle.style.display = "none";
-  title.style.display = "none";
-
+openToggle.addEventListener('click', () => {
+  mobileLinks.classList.toggle('open');
+  openToggle.style.display = 'none';
+  title.style.display = 'none';
+});
+mobileLinks.addEventListener('click', () => {
+  openToggle.style.display = 'block';
+  title.style.display = 'block';
+  mobileLinks.classList.toggle('open');
 });
