381abf41964dd2bcfac762c16485fb90d6783912
Nyame-Wolf <mumenyam@gmail.com>
2022-06-16 18:00:27 +0300

    Add contact form js validation container. Add basic javascript to activate container

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const openToggle = document.getElementById('open-toggle');
-const title = document.getElementById('title');
-const mobileLinks = document.getElementById('mobile-links');
+const openToggle = document.getElementById("open-toggle");
+const title = document.getElementById("title");
+const mobileLinks = document.getElementById("mobile-links");
 
-openToggle.addEventListener('click', () => {
-  mobileLinks.classList.toggle('open');
-  openToggle.style.display = 'none';
-  title.style.display = 'none';
+openToggle.addEventListener("click", () => {
+  mobileLinks.classList.toggle("open");
+  openToggle.style.display = "none";
+  title.style.display = "none";
 });
-mobileLinks.addEventListener('click', () => {
-  openToggle.style.display = 'block';
-  title.style.display = 'block';
-  mobileLinks.classList.toggle('open');
+mobileLinks.addEventListener("click", () => {
+  openToggle.style.display = "block";
+  title.style.display = "block";
+  mobileLinks.classList.toggle("open");
 });
+
+const email = document.getElementById("user-email").value;
+const errM = document.querySelector(".errorMessage");
+
+const message = "";
+myRegex = /[A-Z]/;
+if (myRegex.test(email)) {
+  message = "Email field should only have <strong>lowercase</strong> letters";
+}
