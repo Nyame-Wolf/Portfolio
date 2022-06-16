cc651ddc059d2538793e30c201ce3a1f4d99c1f3
Nyame-Wolf <mumenyam@gmail.com>
2022-06-16 11:11:01 +0300

    add mobile version content dynamism

--- script.js
+++ script.js
@@ -13,6 +13,189 @@ mobileLinks.addEventListener("click", () => {
   mobileLinks.classList.toggle("open");
 });
 
+function createLinks(array) {
+  let list = "";
+  array.forEach((item) => {
+    list += `<li> <a  class="is-link" href="#">${item}</a></li>`;
+  });
+  return list;
+}
+// function createLinks(array) {
+//   const list = document.createElement("ul");
+//   array.forEach((item) => {
+//     list.innerHTML = `<li> <a  class="is-link" href="#">${item}</a></li>`;
+//   });
+//   return list;
+// }
+
+/* grid first article */
+const articleOne = {
+  sectionTitle: "My Recent Works",
+  sectionImage: {
+    src: "assets/ImgPlaceholder@2x.png",
+    alt: "section image",
+  },
+  articleOneTitle: "Multi-Post Stories",
+  itemParagraph: `A daily selection of privately personalized reads; no accounts or
+              sign-ups required. has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a standard
+              dummy text.`,
+  technologyA1: ["css", "html", "bootstrap", "Ruby"],
+  projectBtn: "See Project",
+};
+
+const sTitle = document.querySelector(".works-title");
+const article1Title = document.querySelector(".item1-description");
+
+const sPopupTitle = document.createElement("H2");
+const titleHr = document.createElement("hr");
+const sTitleTxt = document.createTextNode(`${articleOne.sectionTitle}`);
+sPopupTitle.appendChild(sTitleTxt);
+sTitle.appendChild(sPopupTitle);
+sTitle.appendChild(titleHr);
+
+const imageItem1 = document.createElement("IMG");
+imageItem1.setAttribute("src", articleOne.sectionImage.src);
+imageItem1.setAttribute("alt", articleOne.sectionImage.src);
+document.querySelector(".sectionImg").appendChild(imageItem1);
+const ulLinks = document.querySelector(".button-light");
+
+const a1Title = document.createElement("h3");
+const aTitleTxt = document.createTextNode(`${articleOne.articleOneTitle}`);
+a1Title.appendChild(aTitleTxt);
+article1Title.insertBefore(a1Title, ulLinks);
+
+const a1P = document.createElement("p");
+const a1PTxt = document.createTextNode(`${articleOne.itemParagraph}`);
+a1P.appendChild(a1PTxt);
+article1Title.insertBefore(a1P, ulLinks);
+
+ulLinks.innerHTML = createLinks(articleOne.technologyA1);
+
+const buttonSm = document.querySelector(".button-orange-sm");
+
+const buttonSmall = document.createElement("button");
+const buttonSmallTxt = document.createTextNode(`${articleOne.projectBtn}`);
+buttonSmall.type = "submit";
+buttonSmall.appendChild(buttonSmallTxt);
+buttonSm.appendChild(buttonSmall);
+buttonSmall.classList.add("is-link");
+
+// gridProjects
+const articles = [
+  {
+    title: "Profesional Art Printing Data",
+    paragraph: `A daily selection of privately personalized reads; no accounts
+                or sign-ups required. has been the industry's standard`,
+    technologyArticle: ["html", "bootstrap", "Ruby"],
+    prjBtn: "See Project",
+  },
+  {
+    title: "Profesional Art Printing Data",
+    paragraph: `A daily selection of privately personalized reads; no accounts
+                or sign-ups required. has been the industry's standard`,
+    technologyArticle: ["html", "bootstrap", "Ruby"],
+    prjBtn: "See Project",
+  },
+  {
+    title: "Profesional Art Printing Data",
+    paragraph: `A daily selection of privately personalized reads; no accounts
+                or sign-ups required. has been the industry's standard`,
+    technologyArticle: ["html", "bootstrap", "Ruby"],
+    prjBtn: "See Project",
+  },
+  {
+    title: "Profesional Art Printing Data",
+    paragraph: `A daily selection of privately personalized reads; no accounts
+                or sign-ups required. has been the industry's standard`,
+    technologyArticle: ["html", "bootstrap", "Ruby"],
+    prjBtn: "See Project",
+  },
+  {
+    title: "Profesional Art Printing Data",
+    paragraph: `A daily selection of privately personalized reads; no accounts
+                or sign-ups required. has been the industry's standard`,
+    technologyArticle: ["html", "bootstrap", "Ruby"],
+    prjBtn: "See Project",
+  },
+  {
+    title: "Profesional Art Printing Data",
+    paragraph: `A daily selection of privately personalized reads; no accounts
+                or sign-ups required. has been the industry's standard`,
+    technologyArticle: ["html", "bootstrap", "Ruby"],
+    prjBtn: "See Project",
+  },
+];
+
+function createArticleLinks(techTags) {
+  const ul = document.createElement("ul");
+  ul.classList.add("link", "button-dark", "link-item");
+
+  techTags.forEach((tag) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.classList.add("is-link");
+
+    a.textContent = tag;
+
+    li.appendChild(a);
+    ul.appendChild(li);
+  });
+
+  return ul;
+}
+function createH4(h) {
+  const h4 = document.createElement("h4");
+  h4.textContent = h;
+  return h4;
+}
+
+function createp(para) {
+  const p = document.createElement("p");
+  p.textContent = para;
+  return p;
+}
+
+function createButton(cbutton) {
+  const button = document.createElement("button");
+  button.classList.add("button-orange", "is-link");
+  button.textContent = cbutton;
+  return button;
+}
+function createArticle(articleContent) {
+  const article = document.createElement("article");
+  article.classList.add("works");
+
+  const worksDescription = document.createElement("div");
+  worksDescription.classList.add("works-description");
+
+  const h4tag = createH4(articleContent.title);
+  const para = createp(articleContent.paragraph);
+  const links = createArticleLinks(articleContent.technologyArticle);
+  const buttonOrange = createButton(articleContent.prjBtn);
+
+  worksDescription.appendChild(h4tag);
+  worksDescription.appendChild(para);
+  worksDescription.appendChild(links);
+  worksDescription.appendChild(buttonOrange);
+
+  article.appendChild(worksDescription);
+
+  return article;
+}
+
+function createArticles(content) {
+  const grid = document.querySelector(".works-grid");
+
+  content.forEach((article) => {
+    const art = createArticle(article);
+    grid.appendChild(art);
+  });
+}
+
+createArticles(articles);
+
+// modal popup
 const modal = document.querySelector(".dsk-width");
 const modalBtns = document.querySelectorAll(".button-orange");
 const closeBtn = document.getElementsByClassName("close-btn")[0];
@@ -56,20 +239,13 @@ details.forEach((detail) => {
   popupTitle.appendChild(titleTxt);
   desktopTitle.appendChild(popupTitle);
 
-  let list = "";
-  function createLinks(array) {
-    array.forEach((item) => {
-      list += `<li> <a  class="is-link" href="#">${item}</a></li>`;
-    });
-    return list;
-  }
   document.querySelector(".dsk-popup-links").innerHTML = createLinks(
     detail.technologies
   );
 
   const image = document.createElement("IMG");
-  image.setAttribute("src", details[0].featuredImage.src);
-  image.setAttribute("alt", details[0].featuredImage.src);
+  image.setAttribute("src", detail.featuredImage.src);
+  image.setAttribute("alt", detail.featuredImage.src);
   document.querySelector(".dsk-popup-img").appendChild(image);
 
   const detailP = document.createElement("p");
