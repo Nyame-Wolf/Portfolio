efc2d995eaa12e4d9784c5ae8178fb221f54f527
Nyame-Wolf <mumenyam@gmail.com>
2022-06-16 21:05:27 +0300

    Add style to contact form error message to figma

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-const openToggle = document.getElementById("open-toggle");
-const title = document.getElementById("title");
-const mobileLinks = document.getElementById("mobile-links");
+const openToggle = document.getElementById('open-toggle');
+const title = document.getElementById('title');
+const mobileLinks = document.getElementById('mobile-links');
 
-openToggle.addEventListener("click", () => {
-  mobileLinks.classList.toggle("open");
-  openToggle.style.display = "none";
-  title.style.display = "none";
+openToggle.addEventListener('click', () => {
+  mobileLinks.classList.toggle('open');
+  openToggle.style.display = 'none';
+  title.style.display = 'none';
 });
-mobileLinks.addEventListener("click", () => {
-  openToggle.style.display = "block";
-  title.style.display = "block";
-  mobileLinks.classList.toggle("open");
+mobileLinks.addEventListener('click', () => {
+  openToggle.style.display = 'block';
+  title.style.display = 'block';
+  mobileLinks.classList.toggle('open');
 });
 
 const myRegex = /[A-Z]/;
-const form = document.querySelector(".dsk-form");
-form.addEventListener("submit", (event) => {
+const form = document.querySelector('.dsk-form');
+form.addEventListener('submit', (event) => {
   const { email } = form.elements;
   const emailEntered = email.value;
-  const errM = document.querySelector(".errorMessage");
-  console.log({ event, emailEntered });
+  const errM = document.querySelector('.errorMessage');
   if (myRegex.test(emailEntered) === true) {
     event.preventDefault();
-    const message =
-      "Email field should only have <strong>lowercase</strong> letters";
-    console.log(message);
-    errM.innerHTML = message;
+    const message = 'Email field should only have <strong>lowercase</strong> letters';
+    if (message !== '') {
+      errM.innerHTML = message;
+      errM.style.display = 'block';
+    }
   }
-  //else {
-  //   form.submit();
-  // }
 });
