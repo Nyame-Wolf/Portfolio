b3ab5f2a65c0c598ea7bcd8ca286ff7a2c35467a
Nyame-Wolf <mumenyam@gmail.com>
2022-06-17 08:48:35 +0300

    Add article object items

--- script.js
+++ script.js
@@ -87,43 +87,127 @@ const articles = [
     title: "Profesional Art Printing Data",
     paragraph: `A daily selection of privately personalized reads; no accounts
                 or sign-ups required. has been the industry's standard`,
+    featuredImage: {
+      src: "assets/Snapshoot Portfolio.png",
+      alt: "featured image",
+    },
     technologyArticle: ["html", "bootstrap", "Ruby"],
     prjBtn: "See Project",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it 1960s with the releaLorem Ipsum is simply
+              dummy text of the printing and typesetting industry. Lorem Ipsum
+              has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it
+              1960s with the releax map lapora verita.`,
+    liveVersion: "See live",
+    source: "See Source",
   },
   {
     title: "Profesional Art Printing Data",
     paragraph: `A daily selection of privately personalized reads; no accounts
                 or sign-ups required. has been the industry's standard`,
+    featuredImage: {
+      src: "assets/Snapshoot Portfolio.png",
+      alt: "featured image",
+    },
     technologyArticle: ["html", "bootstrap", "Ruby"],
     prjBtn: "See Project",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it 1960s with the releaLorem Ipsum is simply
+              dummy text of the printing and typesetting industry. Lorem Ipsum
+              has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it
+              1960s with the releax map lapora verita.`,
+    liveVersion: "See live",
+    source: "See Source",
   },
   {
     title: "Profesional Art Printing Data",
     paragraph: `A daily selection of privately personalized reads; no accounts
                 or sign-ups required. has been the industry's standard`,
+    featuredImage: {
+      src: "assets/Snapshoot Portfolio.png",
+      alt: "featured image",
+    },
     technologyArticle: ["html", "bootstrap", "Ruby"],
     prjBtn: "See Project",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it 1960s with the releaLorem Ipsum is simply
+              dummy text of the printing and typesetting industry. Lorem Ipsum
+              has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it
+              1960s with the releax map lapora verita.`,
+    liveVersion: "See live",
+    source: "See Source",
   },
   {
     title: "Profesional Art Printing Data",
     paragraph: `A daily selection of privately personalized reads; no accounts
                 or sign-ups required. has been the industry's standard`,
+    featuredImage: {
+      src: "assets/Snapshoot Portfolio.png",
+      alt: "featured image",
+    },
     technologyArticle: ["html", "bootstrap", "Ruby"],
     prjBtn: "See Project",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it 1960s with the releaLorem Ipsum is simply
+              dummy text of the printing and typesetting industry. Lorem Ipsum
+              has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it
+              1960s with the releax map lapora verita.`,
+    liveVersion: "See live",
+    source: "See Source",
   },
   {
     title: "Profesional Art Printing Data",
     paragraph: `A daily selection of privately personalized reads; no accounts
                 or sign-ups required. has been the industry's standard`,
+    featuredImage: {
+      src: "assets/Snapshoot Portfolio.png",
+      alt: "featured image",
+    },
     technologyArticle: ["html", "bootstrap", "Ruby"],
     prjBtn: "See Project",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it 1960s with the releaLorem Ipsum is simply
+              dummy text of the printing and typesetting industry. Lorem Ipsum
+              has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it
+              1960s with the releax map lapora verita.`,
+    liveVersion: "See live",
+    source: "See Source",
   },
   {
     title: "Profesional Art Printing Data",
     paragraph: `A daily selection of privately personalized reads; no accounts
                 or sign-ups required. has been the industry's standard`,
+    featuredImage: {
+      src: "assets/Snapshoot Portfolio.png",
+      alt: "featured image",
+    },
     technologyArticle: ["html", "bootstrap", "Ruby"],
     prjBtn: "See Project",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it 1960s with the releaLorem Ipsum is simply
+              dummy text of the printing and typesetting industry. Lorem Ipsum
+              has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it
+              1960s with the releax map lapora verita.`,
+    liveVersion: "See live",
+    source: "See Source",
   },
 ];
 
