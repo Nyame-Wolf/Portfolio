fe4f42f2605ec377b143313e175475e401692804
Nyame-Wolf <mumenyam@gmail.com>
2022-06-18 17:05:48 +0300

    Merge conflicts issues

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ mobileLinks.addEventListener('click', () => {
   mobileLinks.classList.toggle('open');
 });
 
-
 const articleOne = {
   sectionTitle: 'My Recent Works',
   sectionImage: {
@@ -359,20 +358,4 @@ modalBtns.forEach((modalBtn) => {
       modal.style.display = 'none';
     });
   });
-
-const myRegex = /[A-Z]/;
-const form = document.querySelector('.dsk-form');
-form.addEventListener('submit', (event) => {
-  const { email } = form.elements;
-  const emailEntered = email.value;
-  const errM = document.querySelector('.errorMessage');
-  if (myRegex.test(emailEntered) === true) {
-    event.preventDefault();
-    const message = 'Email field should only have <strong>lowercase</strong> letters';
-    if (message !== '') {
-      errM.innerHTML = message;
-      errM.style.display = 'block';
-    }
-  }
-
 });
