4c2e1471f174c581142254d0b396025a5d9428fa
Nyame-Wolf <mumenyam@gmail.com>
2022-06-18 17:21:26 +0300

    Merge contact form

--- script.js
+++ script.js
@@ -19,12 +19,16 @@ form.addEventListener('submit', (event) => {
   const { email } = form.elements;
   const emailEntered = email.value;
   const errM = document.querySelector('.errorMessage');
-  if (myRegex.test(emailEntered) === true) {
+  if (myRegex.test(emailEntered) === true)
+  {
     event.preventDefault();
     const message = 'Email field should only have <strong>lowercase</strong> letters';
-    if (message !== '') {
+    if (message !== '')
+    {
       errM.innerHTML = message;
       errM.style.display = 'block';
     }
   }
 });
+
+// end contact form validation 
