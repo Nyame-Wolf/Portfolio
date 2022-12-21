243a9ba7574493f5c2612be488c34690fc765fed
Nyame-Wolf <mumenyam@gmail.com>
2022-12-21 12:03:42 +0300

    fix modal imags heights

--- script.js
+++ script.js
@@ -1,68 +1,69 @@
-const openToggle = document.getElementById('open-toggle');
-const title = document.getElementById('title');
-const mobileLinks = document.getElementById('mobile-links');
+const openToggle = document.getElementById("open-toggle");
+const title = document.getElementById("title");
+const mobileLinks = document.getElementById("mobile-links");
 
-openToggle.addEventListener('click', () => {
-  mobileLinks.classList.toggle('open');
-  openToggle.style.display = 'none';
-  title.style.display = 'none';
+openToggle.addEventListener("click", () => {
+  mobileLinks.classList.toggle("open");
+  openToggle.style.display = "none";
+  title.style.display = "none";
 });
-mobileLinks.addEventListener('click', () => {
-  openToggle.style.display = 'block';
-  title.style.display = 'block';
-  mobileLinks.classList.toggle('open');
+mobileLinks.addEventListener("click", () => {
+  openToggle.style.display = "block";
+  title.style.display = "block";
+  mobileLinks.classList.toggle("open");
 });
 
 const myRegex = /[A-Z]/;
-const form = document.querySelector('.dsk-form');
-form.addEventListener('submit', (event) => {
+const form = document.querySelector(".dsk-form");
+form.addEventListener("submit", (event) => {
   const { email } = form.elements;
   const emailEntered = email.value;
-  const errM = document.querySelector('.errorMessage');
+  const errM = document.querySelector(".errorMessage");
   if (myRegex.test(emailEntered) === true) {
     event.preventDefault();
-    const message = 'Email field should only have <strong>lowercase</strong> letters';
-    if (message !== '') {
+    const message =
+      "Email field should only have <strong>lowercase</strong> letters";
+    if (message !== "") {
       errM.innerHTML = message;
-      errM.style.display = 'block';
+      errM.style.display = "block";
     }
   }
 });
 const articleOne = {
-  sectionTitle: 'My Recent Works',
+  sectionTitle: "My Recent Works",
   sectionImage: {
-    src: 'assets/Screenshot 2022-12-20 at 01.10.46.png',
-    alt: 'section image',
+    src: "assets/Screenshot 2022-12-20 at 01.10.46.png",
+    alt: "section image",
   },
 };
-const sTitle = document.querySelector('.works-title');
+const sTitle = document.querySelector(".works-title");
 
-const sPopupTitle = document.createElement('H2');
-const titleHr = document.createElement('hr');
+const sPopupTitle = document.createElement("H2");
+const titleHr = document.createElement("hr");
 const sTitleTxt = document.createTextNode(`${articleOne.sectionTitle}`);
 sPopupTitle.appendChild(sTitleTxt);
 sTitle.appendChild(sPopupTitle);
 sTitle.appendChild(titleHr);
 
-const imageItem1 = document.createElement('IMG');
-imageItem1.setAttribute('src', articleOne.sectionImage.src);
-imageItem1.setAttribute('alt', articleOne.sectionImage.src);
-document.querySelector('.sectionImg').appendChild(imageItem1);
+const imageItem1 = document.createElement("IMG");
+imageItem1.setAttribute("src", articleOne.sectionImage.src);
+imageItem1.setAttribute("alt", articleOne.sectionImage.src);
+document.querySelector(".sectionImg").appendChild(imageItem1);
 
 // gridProjects
 const articles = [
   {
-    title: 'Crypto Prism',
-    closeBtn: '&#x3A7',
+    title: "Crypto Prism",
+    closeBtn: "&#x3A7",
     paragraph: `This metric app uses the coinlore api to display a list of crypto 
     currencies with their current price. It has a detail page that shows details 
     such as crypto symbol and its percentage daily change. `,
     featuredImage: {
-      src: 'assets/Screenshot 2022-12-20 at 01.10.46.png',
-      alt: 'featured image',
+      src: "assets/Screenshot 2022-12-20 at 01.10.46.png",
+      alt: "featured image",
     },
-    technologyArticle: ['Css', 'Html', 'React', 'Redux'],
-    prjBtn: 'See Project',
+    technologyArticle: ["Css", "Html", "React", "Redux"],
+    prjBtn: "See Project",
     description: `This metric app uses the coinlore api to display a list of crypto 
     currencies with their current price. It has a detail page that shows details 
     such as crypto symbol and its percentage daily change.`,
@@ -73,18 +74,18 @@ const articles = [
   },
 
   {
-    title: 'MovieFlix',
-    closeBtn: '&#x3A7',
+    title: "MovieFlix",
+    closeBtn: "&#x3A7",
     paragraph: `This is a web application that consumes the tvmaze API. 
     It shows a list of 240 popular, TV shows . It allows users to add a 
     comment about the movie as well as click on the like button if they 
     enjoyed it`,
     featuredImage: {
-      src: 'assets/movieflix.png',
-      alt: 'featured image',
+      src: "assets/movieflix.png",
+      alt: "featured image",
     },
-    technologyArticle: ['html', 'css', 'Javascript'],
-    prjBtn: 'See Project',
+    technologyArticle: ["html", "css", "Javascript"],
+    prjBtn: "See Project",
     description: `This is a web application that consumes the tvmaze API. 
     It shows a list of 240 popular, TV shows . It allows users to add a 
     comment about the movie as well as click on the like button if they 
@@ -95,17 +96,17 @@ const articles = [
       '<a href="https://github.com/Nyame-Wolf/movie-app" target="_blank" rel="noreferrer">See Source</a>',
   },
   {
-    title: 'Budget App',
-    closeBtn: '&#x3A7',
+    title: "Budget App",
+    closeBtn: "&#x3A7",
     paragraph: `This app lets you manage your spending. You can add a 
     category of items you want to spend and the app aggregates your net 
     spend`,
     featuredImage: {
-      src: 'assets/budget.jpg',
-      alt: 'featured image',
+      src: "assets/budget.jpg",
+      alt: "featured image",
     },
-    technologyArticle: ['html', 'Ruby on Rails', 'Ruby'],
-    prjBtn: 'See Project',
+    technologyArticle: ["html", "Ruby on Rails", "Ruby"],
+    prjBtn: "See Project",
     description: `This app lets you manage your spending. You can add a 
     category of items you want to spend and the app aggregates your net 
     spend`,
@@ -115,17 +116,17 @@ const articles = [
       '<a href="https://github.com/Nyame-Wolf/budget-app" target="_blank" rel="noreferrer">See Source</a>',
   },
   {
-    title: 'Todo List',
-    closeBtn: '&#x3A7',
+    title: "Todo List",
+    closeBtn: "&#x3A7",
     paragraph: `This is a todo list created using webpack and vanilla javascript. 
     It lets a user add and remove to do items. It contains testing suite 
     using jest framework`,
     featuredImage: {
-      src: 'assets/todo.png',
-      alt: 'featured image',
+      src: "assets/todoedit.png",
+      alt: "featured image",
     },
-    technologyArticle: ['html', 'Javascript', 'Css'],
-    prjBtn: 'See Project',
+    technologyArticle: ["html", "Javascript", "Css"],
+    prjBtn: "See Project",
     description: `This is a todo list created using webpack and vanilla javascript. 
     It lets a user add and remove to do items. It contains testing suite 
     using jest framework`,
@@ -135,17 +136,17 @@ const articles = [
       '<a href="https://github.com/Nyame-Wolf/To-Do-list" target="_blank" rel="noreferrer">See Source</a>',
   },
   {
-    title: 'Space Missions',
-    closeBtn: '&#x3A7',
+    title: "Space Missions",
+    closeBtn: "&#x3A7",
     paragraph: `This project is a web application that consumes the SpaceX API. It 
     provides commercial and scientific space travel services that allows users to 
     book rockets and join selected space missions`,
     featuredImage: {
-      src: 'assets/rockets.png',
-      alt: 'featured image',
+      src: "assets/rockets.png",
+      alt: "featured image",
     },
-    technologyArticle: ['html', 'React', 'css'],
-    prjBtn: 'See Project',
+    technologyArticle: ["html", "React", "css"],
+    prjBtn: "See Project",
     description: `This project is a web application that consumes the SpaceX API. It 
     provides commercial and scientific space travel services that allows users to 
     book rockets and join selected space missions.`,
@@ -155,18 +156,18 @@ const articles = [
       '<a href=" https://github.com/Nyame-Wolf/space-missions" target="_blank" rel="noreferrer">See Source</a>',
   },
   {
-    title: 'Exploration Getaways',
-    closeBtn: '&#x3A7',
+    title: "Exploration Getaways",
+    closeBtn: "&#x3A7",
     paragraph: `Exploration Getaways is a travelling application where 
     the user can register new accounts, login and book a set of different 
     trip packages. It is built and connected by using two different repos, 
     including back-end(Rails) and front-end(React/Redux)`,
     featuredImage: {
-      src: 'assets/explore.jpg',
-      alt: 'featured image',
+      src: "assets/explore.jpg",
+      alt: "featured image",
     },
-    technologyArticle: ['html', 'React', 'css', 'Ruby on Rails'],
-    prjBtn: 'See Project',
+    technologyArticle: ["html", "React", "css", "Ruby on Rails"],
+    prjBtn: "See Project",
     description: `Exploration Getaways is a travelling application where 
     the user can register new accounts, login and book a set of different 
     trip packages. It is built and connected by using two different repos, 
@@ -177,17 +178,17 @@ const articles = [
       '<a href=" https://github.com/Nyame-Wolf/Exploration-Getaways-FRONTEND" target="_blank" rel="noreferrer">See Source</a>',
   },
   {
-    title: 'Holistic Med Annual Conference',
-    closeBtn: '&#x3A7',
+    title: "Holistic Med Annual Conference",
+    closeBtn: "&#x3A7",
     paragraph: `This is inspired by 2015 by Cindy Shin ccGlobal Korea. 
     It is a website for an imaginary annual holistic medicine conference. 
     It is made using plain vanilla javascript, HTML and css.`,
     featuredImage: {
-      src: 'assets/holistic-med.png',
-      alt: 'featured image',
+      src: "assets/holistic-med.png",
+      alt: "featured image",
     },
-    technologyArticle: ['html', 'css', 'Javascript'],
-    prjBtn: 'See Project',
+    technologyArticle: ["html", "css", "Javascript"],
+    prjBtn: "See Project",
     description: `This is inspired by 2015 by Cindy Shin ccGlobal Korea. 
     It is a website for an imaginary annual holistic medicine conference. 
     It is made using plain vanilla javascript, HTML and css.`,
@@ -199,13 +200,13 @@ const articles = [
 ];
 
 function createArticleLinks(techTags) {
-  const ul = document.createElement('ul');
-  ul.classList.add('link', 'button-dark', 'link-item');
+  const ul = document.createElement("ul");
+  ul.classList.add("link", "button-dark", "link-item");
 
   techTags.forEach((tag) => {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    a.classList.add('is-link');
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.classList.add("is-link");
 
     a.textContent = tag;
 
@@ -218,44 +219,44 @@ function createArticleLinks(techTags) {
 // popup only items
 
 function image() {
-  const img = document.createElement('img');
+  const img = document.createElement("img");
   return img;
 }
 
 function description(d) {
-  const p = document.createElement('p');
+  const p = document.createElement("p");
   p.textContent = d;
   return p;
 }
 
 // popuponly items end
 function createH4(h) {
-  const h4 = document.createElement('h4');
+  const h4 = document.createElement("h4");
   h4.textContent = h;
   return h4;
 }
 
 function createp(para) {
-  const p = document.createElement('p');
+  const p = document.createElement("p");
   p.textContent = para;
   return p;
 }
 
 function createButton(cbutton) {
-  const button = document.createElement('button');
-  button.classList.add('button-orange', 'is-link', 'grid-buttons');
+  const button = document.createElement("button");
+  button.classList.add("button-orange", "is-link", "grid-buttons");
   button.textContent = cbutton;
   return button;
 }
 
 function createArticle(articleContent, index) {
-  const article = document.createElement('article');
-  article.classList.add('works');
-  article.classList.add('bkg');
-  article.setAttribute('data-index', index);
+  const article = document.createElement("article");
+  article.classList.add("works");
+  article.classList.add("bkg");
+  article.setAttribute("data-index", index);
 
-  const worksDescription = document.createElement('div');
-  worksDescription.classList.add('works-description', 'add-padding');
+  const worksDescription = document.createElement("div");
+  worksDescription.classList.add("works-description", "add-padding");
 
   const h4tag = createH4(articleContent.title);
   const para = createp(articleContent.paragraph);
@@ -271,7 +272,7 @@ function createArticle(articleContent, index) {
 
   return article;
 }
-const grid = document.querySelector('.works-grid');
+const grid = document.querySelector(".works-grid");
 
 function createArticles(content) {
   content.forEach((article, index) => {
@@ -284,69 +285,69 @@ createArticles(articles);
 
 // article 1 styles:
 
-const a = document.querySelector('.item1-description');
-const rmBkg = document.querySelectorAll('.works')[0];
-rmBkg.classList.toggle('bkg');
-rmBkg.classList.add('cchange');
-const article1Ul = rmBkg.querySelector('ul');
-article1Ul.classList.add('button-light');
-const article1Btn = rmBkg.querySelector('button');
-article1Btn.classList.toggle('grid-buttons');
-article1Btn.classList.add('button-orange-sm');
-const article1div = rmBkg.querySelector('div');
-article1div.classList.toggle('add-padding');
+const a = document.querySelector(".item1-description");
+const rmBkg = document.querySelectorAll(".works")[0];
+rmBkg.classList.toggle("bkg");
+rmBkg.classList.add("cchange");
+const article1Ul = rmBkg.querySelector("ul");
+article1Ul.classList.add("button-light");
+const article1Btn = rmBkg.querySelector("button");
+article1Btn.classList.toggle("grid-buttons");
+article1Btn.classList.add("button-orange-sm");
+const article1div = rmBkg.querySelector("div");
+article1div.classList.toggle("add-padding");
 
 a.appendChild(grid.firstChild);
 
 // modal popup
-const modal = document.querySelector('.dsk-width');
-const modalBtns = document.querySelectorAll('.button-orange');
+const modal = document.querySelector(".dsk-width");
+const modalBtns = document.querySelectorAll(".button-orange");
 
 modalBtns.forEach((modalBtn) => {
-  modalBtn.addEventListener('click', (event) => {
+  modalBtn.addEventListener("click", (event) => {
     const parentArticle = event.target.parentElement.parentElement;
     const articleIndex = parentArticle.dataset.index;
     const article = articles[articleIndex];
 
-    const popUp = document.createElement('div');
-    popUp.classList.add('popup');
+    const popUp = document.createElement("div");
+    popUp.classList.add("popup");
 
-    const closeButton = document.createElement('button');
+    const closeButton = document.createElement("button");
     closeButton.innerHTML = `${article.closeBtn}`;
-    closeButton.classList.add('close-btn');
+    closeButton.classList.add("close-btn");
 
     const h4tagModal = createH4();
     h4tagModal.innerHTML = `<h4 class="popup-title">${article.title}</h4>`;
 
     const links = createArticleLinks(article.technologyArticle);
-    links.classList.remove('button-dark', 'link-item');
-    links.classList.add('popup-links');
+    links.classList.remove("button-dark", "link-item");
+    links.classList.add("popup-links");
 
-    const imageText = document.createElement('div');
-    imageText.classList.add('image-text');
+    const imageText = document.createElement("div");
+    imageText.classList.add("image-text");
 
-    const popUpimage = document.createElement('div');
-    popUpimage.classList.add('popup-img');
+    const popUpimage = document.createElement("div");
+    popUpimage.classList.add("popup-img");
     const imgC = image(article.featuredImage);
     imgC.src = article.featuredImage.src;
     imgC.alt = article.featuredImage.alt;
     popUpimage.appendChild(imgC);
 
-    const buttonText = document.createElement('div');
-    buttonText.classList.add('button-txt');
+    const buttonText = document.createElement("div");
+    buttonText.classList.add("button-txt");
     const des = description(article.description);
-    des.classList.add('popup-txt');
+    des.classList.add("popup-txt");
 
-    const popupButtons = document.createElement('div');
-    popupButtons.classList.add('popup-buttons');
+    const popupButtons = document.createElement("div");
+    popupButtons.classList.add("popup-buttons");
 
-    const popButtonLive = document.createElement('button');
+    const popButtonLive = document.createElement("button");
     popButtonLive.innerHTML = `<button type="submit" class="is-link popup-btn"><span>${article.liveVersion}</span>    <i class="fa fa-check-circle-o" aria-hidden="true"></i></span></button>`;
-    popButtonLive.classList.add('live');
+    popButtonLive.classList.add("live");
 
-    const popButtonSource = document.createElement('button');
+    const popButtonSource = document.createElement("button");
     popButtonSource.innerHTML = `<button type="submit" class="is-link popup-btn"><span>${article.source}</span>    <span><i class="fa fa-github" aria-hidden="true"></span></button>`;
-    popButtonSource.classList.add('source');
+    popButtonSource.classList.add("source");
     popupButtons.appendChild(popButtonLive);
     popupButtons.appendChild(popButtonSource);
 
@@ -363,13 +364,13 @@ modalBtns.forEach((modalBtn) => {
 
     modal.appendChild(popUp);
 
-    modal.classList.add('openModel');
-    modal.style.display = 'block';
+    modal.classList.add("openModel");
+    modal.style.display = "block";
 
-    closeButton.addEventListener('click', () => {
-      modal.classList.remove('openModel');
+    closeButton.addEventListener("click", () => {
+      modal.classList.remove("openModel");
       popUp.remove();
-      modal.style.display = 'none';
+      modal.style.display = "none";
     });
   });
 });
